910d209455010b64b4cb463045814a4667bc4c25
Aaron Snoswell <aaronsnoswell@gmail.com>
2018-05-14 17:17:19 +1000

    Place holders for max_margin and projection IRL

---
+++ irl_methods/max_margin.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""Implementation of max-margin IRL by Abbeel and Ng, 2004
+
+Copyright 2018 Aaron Snoswell
+"""
+
+import math
+import numpy as np
+
+from cvxopt import matrix, solvers
+
+
+def max_margin(verbose=False):
+    """Maximum-margin IRL by Abbeel and Ng, 2004
+    
+    Args:
+    
+    Returns:
+    """
+
+    return None
+
---
+++ irl_methods/projection.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""Implementation of projection IRL by Abbeel and Ng, 2004
+
+Copyright 2018 Aaron Snoswell
+"""
+
+import math
+import numpy as np
+
+from cvxopt import matrix, solvers
+
+
+def projection(verbose=False):
+    """Projection IRL by Abbeel and Ng, 2004
+    
+    Args:
+    
+    Returns:
+    """
+
+    return None
+
