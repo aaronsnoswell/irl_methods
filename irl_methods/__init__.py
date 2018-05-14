5e6c5172871cc4a7169760de41df61a0b6b51da2
Aaron Snoswell <aaronsnoswell@gmail.com>
2018-05-14 15:54:28 +1000

    Add basis functions to __init__.py

--- irl_methods/__init__.py
+++ irl_methods/__init__.py
@@ -2,10 +2,13 @@
 from .lp import lp
 from .llp import llp
 from .tlp import tlp
+from .basis import gaussian, indicator
 
 __all__ = [
     "lp",
     "llp",
     "tlp",
+    "gaussian",
+    "indicator",
     "examples"
 ]
