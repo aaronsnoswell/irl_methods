a1a7355de82fb7e056f54957cb19d7231d09ec27
Aaron Snoswell <aaronsnoswell@gmail.com>
2018-06-11 20:00:51 +1000

    Add rollout.py

--- irl_methods/utils/__init__.py
+++ irl_methods/utils/__init__.py
@@ -8,12 +8,14 @@ Copyright 2018 Aaron Snoswell
 """
 
 from .basis import gaussian, indicator
+from .rollout import rollout
 from .dacadc import adc, dac
 from .transform import make_gridworld
 from .plot import plot_trajectory_4d
 
 __all__ = [
-    "gaussian", "indicator"
+    "gaussian", "indicator",
+    "rollout",
     "adc", "dac"
     "make_gridworld",
     "plot_trajectory_4d"
---
+++ irl_methods/utils/rollout.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""Roll out a policy to generate trajectories
+
+Copyright 2018 Aaron Snoswell
+"""
+
+import math
+
+
+def rollout(mdp, start_state, policy, *, max_length=math.inf):
+    """Roll out a policy to generate (s, a) trajectories
+
+    Args:
+        mdp (gym.env): MDP instance
+        start_state (any): Starting state for the rollout
+        policy (function): Policy p(s) -> a
+
+        max_length: Maximum trajectory length
+
+    Returns:
+        (list): A single trajectory, as list of (s, a) pairs
+        (list): A list of the rewards received after each action
+    """
+
+    state = start_state
+    trajectory = []
+    rewards = []
+    while True:
+        # Query the policy for an action
+        action = policy(state)
+
+        # Store the (s, a) tuple
+        trajectory.append((state, action))
+
+        # Take that action
+        mdp.reset()
+        mdp.state = state
+        state, reward, done, status = mdp.step(action)
+        rewards.append(reward)
+
+        # Check exit conditions
+        if done or len(trajectory) > max_length:
+            break
+
+    return trajectory, rewards
