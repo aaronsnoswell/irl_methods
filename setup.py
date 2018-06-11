6e94cc2aa4a610a8ba30e8faf0dba812dc3ce35e
Aaron Snoswell <aaronsnoswell@gmail.com>
2018-06-11 10:02:52 +1000

    Add mdp folder to setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,17 @@ from setuptools import setup
 
 setup(name='irl_methods',
       version='0.1.0',
-      description='High-quality implementations of various algorithms for Inverse Reinforcement Learning',
+      description='High-quality implementations of various algorithms for '
+                  'Inverse Reinforcement Learning',
       url='https://github.com/aaronsnoswell/irl_methods',
       author='Aaron Snoswell',
       author_email='aaron.snoswell@uqconnect.edu.au',
       license='MIT',
-      packages=['irl_methods', 'irl_methods.utils', 'irl_methods.thirdparty'],
+      packages=[
+            'irl_methods',
+            'irl_methods.utils',
+            'irl_methods.thirdparty',
+            'irl_methods.mdp'
+      ],
       install_requires=['numpy', 'cvxopt', 'gym'],
       zip_safe=False)
